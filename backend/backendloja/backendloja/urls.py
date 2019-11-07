663fa7c01141c1eff4a2856d97b14228eb4dc80c
AnaRafaela <anarafaela30103@gmail.com>
2019-11-07 09:51:25 -0300

    loja/urls

--- backend/backendloja/backendloja/urls.py
+++ backend/backendloja/backendloja/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls.authtoken')),
-    path('api/', include('loja.urls')),
+    path('loja/', include('loja.urls')),
 ]
--- backend/backendloja/loja/urls.py
+++ backend/backendloja/loja/urls.py
@@ -1,7 +1,8 @@
-from django.conf.urls import url
+from django.urls import path
 
-#from .views import ()
+from .views import (ProdutoList, CategoriaList)
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
+    path('produtos/', ProdutoList.as_view()),
+    path('categorias/', CategoriaList.as_view()),
 ]
