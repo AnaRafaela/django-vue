425529502353fe2283be711d0c299faf2ed37942
AnaRafaela <anarafaela30103@gmail.com>
2019-11-21 08:53:36 -0300

    imagens do bd resolvidogit add .

--- frontend-vue/app/src/components/Index.vue
+++ frontend-vue/app/src/components/Index.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <v-container class="black">
       <div class="d-block pa-2 black">
       <v-carousel cycle :show-arrows="false">
         <v-carousel-item v-for="(item,i) in items" :key="i" :src="item.src"></v-carousel-item>
       </v-carousel>
       <h1 class="text-center">Loja Casa Bonita</h1>
 </div>
+<v-container class="black">
       <v-row no-gutters justify="center">
         <div v-for="produto in produtos" v-bind:key="produto.id">
           <v-col>
@@ -14,17 +14,21 @@
               <v-img
                 height="150px"
                 width="300px"
-                src="../assets/foto/cama.jpeg"
+                :src="produto.foto"
               >
+               <v-row
+               align="center" 
+               justify="end">
                 <v-tooltip top>
                   <template v-slot:activator="{ on }">
-                    <v-btn color="#00695C" dark v-on="on">
+                    <v-btn class="mx-2" color="#00695C" dark v-on="on" fab small>
                       <v-icon dark>mdi-cart-arrow-down</v-icon>
                     </v-btn>
                   </template>
                   <span>Adicionar ao carrinho e continuar comprando</span>
                 </v-tooltip>
-                {{produto.foto}}
+               </v-row>
+                {{produto.foto.url}}
               </v-img>
               <v-card-title>{{produto.nome}}</v-card-title>
               <v-card-subtitle>
@@ -49,7 +53,7 @@
           </v-col>
         </div>
       </v-row>
-    </v-container>
+      </v-container>
     </div>
 </template>
 
